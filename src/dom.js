90ca947ceefe7ba3a57007a78c7494b2811cbc20
ophanim777 <rikyjo49@gmail.com>
2026-01-18 11:15:29 +0700

    make default project if data in localstorage empty

--- src/dom.js
+++ src/dom.js
@@ -2,3 +2,7 @@ import { Project } from './projects';
 import { Todo } from './todos';
 import { saveProjects, loadProjects } from './storage';
 let projects = loadProjects();
+if (projects.length === 0) {
+    projects.push(new Project('Default'));
+    saveProjects(projects);
+}
