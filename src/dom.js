484205e4e748f1dd06847d74ac3b678cb2e383e3
ophanim777 <rikyjo49@gmail.com>
2026-01-18 11:19:51 +0700

    add function to render todo

--- src/dom.js
+++ src/dom.js
@@ -18,3 +18,14 @@ export function renderProjects(container) {
         container.appendChild(div);
     });
 }
+
+export function renderTodos(project, container) {
+    container.innerHTML = '';
+    project.todos.forEach((todo, i) => {
+        const div = document.createElement('div');
+        div.textContent = `${todo.title} - ${todo.dueDate}`;
+        div.style.color = todo.priority === 'high' ? 'red' : todo.priority === 'medium' ? 'orange' : 'green';
+        div.addEventListener('click', () => alert(JSON.stringify(todo, null, 2)));
+        container.appendChild(div);
+    });
+}
